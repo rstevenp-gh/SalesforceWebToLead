34236f0c840a64b35802cae1014ff25565f101f2
rstevenp-gh <rstevenp@yahoo.com>
2023-09-28 17:48:41 -0500

    Added recaptcha

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ function beforeSubmit() {
     let datePicker = document.getElementById('date-of-birth');
     let hiddenDate = document.getElementById('Date_of_Birth__c');
 
-       
+      
 
     let formattedDate = new Date(datePicker.value + " ").toLocaleDateString("en-US");
     hiddenDate.value = formattedDate;
-
-
-    
-    
 }
+
+
+    function timestamp() {
+         var response = document.getElementById("g-recaptcha-response");
+          if (response == null || response.value.trim() == "") {
+            var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+            elems["ts"] = JSON.stringify(new Date().getTime());
+            document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
+         } 
+    } 
+    
+    setInterval(timestamp, 500); 
+    
